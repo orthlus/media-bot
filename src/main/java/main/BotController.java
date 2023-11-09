f4e6d1a0495f1a8b49812e46921628dfcd75451f
ort <orthodoxplus@ya.ru>
2023-11-09 07:13:21 +0300

    fix

--- src/main/java/main/BotController.java
+++ src/main/java/main/BotController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -20,7 +20,7 @@ public class BotController {
 	@Value("${bot.secret}")
 	private String botSecret;
 
-	@GetMapping("/telegram/bot")
+	@PostMapping("/telegram/bot")
 	public ResponseEntity<BotApiMethod<?>> update(@RequestBody Update update, HttpServletRequest request) {
 		if (validSecret(request)) {
 			bot.onWebhookUpdateReceived(update);
