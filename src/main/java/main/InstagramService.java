310363294fce9cd26c2acd8ed64de8517cbff98c
ort <orthodoxplus@ya.ru>
2023-11-09 04:43:05 +0300

    ig

--- src/main/java/main/InstagramService.java
+++ src/main/java/main/InstagramService.java
@@ -1,4 +1,20 @@
 package main;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.util.Optional;
+
+@Component
 public class InstagramService {
+	@Value("${instagram.api.token}")
+	private String apiToken;
+	@Value("${instagram.api.url}")
+	private String apiUrl;
+
+
+	public Optional<String> getMediaUrl(URI uri) {
+		return null;
+	}
 }
