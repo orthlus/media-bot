003c51249a262fe356028c677a9c6b19a1127acd
ort <orthodoxplus@ya.ru>
2024-06-19 00:53:59 +0300

    some moves to helpers

--- src/main/java/main/TelegramConfig.java
+++ src/main/java/main/TelegramConfig.java
@@ -1,49 +1,35 @@
 package main;
 
 import art.aelaort.SpringLongPollingBot;
+import art.aelaort.TelegramClientBuilder;
 import art.aelaort.TelegramInit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
-import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
-import org.telegram.telegrambots.meta.TelegramUrl;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 import java.util.List;
-import java.util.function.Supplier;
+
+import static art.aelaort.TelegramBots.createTelegramInit;
+import static art.aelaort.TelegramBots.telegramUrlSupplier;
 
 @Configuration
 public class TelegramConfig {
-	@Value("${telegram.api.url.schema}")
-	private String schema;
-	@Value("${telegram.api.url.host}")
-	private String host;
-	@Value("${telegram.api.url.port}")
-	private int port;
+	@Value("${telegram.api.url}")
+	private String telegramUrl;
 	@Value("${bot.token}")
 	private String botToken;
 
 	@Bean
-	public TelegramClient telegramClient(Supplier<TelegramUrl> telegramUrlSupplier) {
-		return new OkHttpTelegramClient(botToken, telegramUrlSupplier.get());
+	public TelegramClient telegramClient() {
+		return TelegramClientBuilder.builder()
+				.token(botToken)
+				.telegramUrlSupplier(telegramUrlSupplier(telegramUrl))
+				.build();
 	}
-
-	@Bean
-	public Supplier<TelegramUrl> telegramUrlSupplier() {
-		if (schema.equals("none")) {
-			return () -> TelegramUrl.DEFAULT_URL;
-		}
-		return () -> new TelegramUrl(schema, host, port);
-	}
-
-	@Bean
-    public TelegramBotsLongPollingApplication telegramBotsApplication() {
-        return new TelegramBotsLongPollingApplication();
-    }
 
 	@Bean
 	public TelegramInit telegramInit(List<SpringLongPollingBot> bots) {
-		return new TelegramInit(telegramBotsApplication(), bots, telegramUrlSupplier());
+		return createTelegramInit(bots, telegramUrlSupplier(telegramUrl));
 	}
 }
