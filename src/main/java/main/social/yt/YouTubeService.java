d3fcab83ecdc512f826cc54e99da4b130fa1ae95
ort <orthodoxplus@ya.ru>
2025-01-16 20:49:49 +0300

    fix autoclosable

--- src/main/java/main/social/yt/YouTubeService.java
+++ src/main/java/main/social/yt/YouTubeService.java
@@ -19,6 +19,7 @@ import java.nio.file.attribute.FileTime;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 @Slf4j
 @Component
@@ -31,18 +32,18 @@ public class YouTubeService {
 
 	@Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
 	public void cleanup() {
-		try {
-			Files.walk(Path.of(ytdlpDir), 1)
-					.filter(path -> path.toFile().isFile())
-					.filter(this::isFileOlder5Hours)
-					.forEach(path -> {
-						try {
-							Files.deleteIfExists(path);
-							log.info("deleted file {}", path);
-						} catch (IOException e) {
-							throw new RuntimeException(e);
-						}
-					});
+		try (Stream<Path> walk = Files.walk(Path.of(ytdlpDir), 1)) {
+				walk
+						.filter(path -> path.toFile().isFile())
+						.filter(this::isFileOlder5Hours)
+						.forEach(path -> {
+							try {
+								Files.deleteIfExists(path);
+								log.info("deleted file {}", path);
+							} catch (IOException e) {
+								throw new RuntimeException(e);
+							}
+						});
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
