c30030c8d4a45e9eceadb794bfe41aa5254d6db6
ort <orthodoxplus@ya.ru>
2025-03-15 21:32:19 +0300

    vk hosts

--- src/main/java/main/social/KnownHosts.java
+++ src/main/java/main/social/KnownHosts.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum KnownHosts {
 	INSTAGRAM("instagram", "instagram.com"),
 	TIKTOK("tiktok", "tiktok.com", "vt.tiktok.com", "vm.tiktok.com"),
-	VK("vk", "vk.com"),
+	VK("vk", "vk.com", "vkvideo.ru"),
 	YOUTUBE("youtube", "youtube.com", "youtu.be");
 	final String[] hosts;
 	final String text;
