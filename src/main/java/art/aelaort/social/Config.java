a11fb93ca8d8c3de4089c684ebca0e8631ecca5c
ort <orthodoxplus@ya.ru>
2025-04-10 02:52:25 +0300

    proxy config

--- src/main/java/art/aelaort/social/Config.java
+++ src/main/java/art/aelaort/social/Config.java
@@ -4,21 +4,38 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URI;
 import java.time.Duration;
 
 @Configuration
 @EnableRetry
 public class Config {
+	@Value("${proxy-url}")
+	private URI proxyUrl;
+
+	private void proxyCustomizer(RestTemplate restTemplate) {
+		String host = proxyUrl.getHost();
+		int port = proxyUrl.getPort();
+
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		Proxy proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(host, port));
+
+		requestFactory.setProxy(proxy);
+		restTemplate.setRequestFactory(requestFactory);
+	}
+
 	@Bean
 	public RestTemplate ig(RestTemplateBuilder restTemplateBuilder,
-						   ProxyCustomizer proxyCustomizer,
 						   @Value("${instagram.api.token}") String igApiToken,
 						   @Value("${instagram.api.url}") String igApiUrl) {
 		return restTemplateBuilder
-				.customizers(proxyCustomizer)
+				.customizers(this::proxyCustomizer)
 				.rootUri(igApiUrl)
 				.connectTimeout(Duration.ofMinutes(5))
 				.readTimeout(Duration.ofMinutes(5))
@@ -28,11 +45,10 @@ public class Config {
 
 	@Bean
 	public RestTemplate tiktok(RestTemplateBuilder restTemplateBuilder,
-							   ProxyCustomizer proxyCustomizer,
 							   @Value("${tiktok.api.url}") String tiktokApiUrl,
 							   @Value("${tiktok.api.token}") String tiktokApiToken) {
 		return restTemplateBuilder
-//				.customizers(proxyCustomizer)
+//				.customizers(this::proxyCustomizer)
 				.rootUri(tiktokApiUrl)
 				.connectTimeout(Duration.ofMinutes(5))
 				.readTimeout(Duration.ofMinutes(5))
