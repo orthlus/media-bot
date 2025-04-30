223d1490c4e3cdc390d60727c28c919a20746ac0
ort <orthodoxplus@ya.ru>
2025-04-30 20:18:56 +0300

    utils

--- src/main/java/art/aelaort/utils/TelegramUtils.java
+++ src/main/java/art/aelaort/utils/TelegramUtils.java
@@ -1,13 +1,34 @@
 package art.aelaort.utils;
 
-import lombok.SneakyThrows;
 import art.aelaort.exceptions.TooLargeFileException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class TelegramUtils {
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
+	public static Update deserializeUpdate(String updateString) {
+		try {
+			return objectMapper.readValue(updateString, Update.class);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String serializeUpdate(Update update) {
+		try {
+			return objectMapper.writeValueAsString(update);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static void checkFileSize(Path file) throws IOException {
 		if (isFileTooLarge(file)) {
 			Files.deleteIfExists(file);
