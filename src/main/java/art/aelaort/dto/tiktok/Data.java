e248e7b81bc45f81731d8a17cc88294af31e0988
ort <orthodoxplus@ya.ru>
2025-08-19 22:48:40 +0300

    fix tiktok json

--- src/main/java/art/aelaort/dto/tiktok/Data.java
+++ src/main/java/art/aelaort/dto/tiktok/Data.java
@@ -1,8 +1,10 @@
 package art.aelaort.dto.tiktok;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 class Data {
 	@JsonProperty("aweme_details")
+	@JsonAlias("aweme_detail")
 	AwemeDetails[] awemeDetails;
 }
